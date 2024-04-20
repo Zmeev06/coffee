71e84a40e7f0184b388ab7d03edcce70b8ff1c31
ArmJav <91393155+ArmJav@users.noreply.github.com>
2024-04-21 02:05:56 +0300

    feat

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,7 +2,10 @@ import { createApp } from "vue";
 import { router, store } from "./providers";
 import App from "./index.vue";
 import Antd from "ant-design-vue";
+import GoogleSignInPlugin from "vue3-google-signin";
 
-const initializeApp = createApp(App).use(router).use(store).use(Antd);
+const initializeApp = createApp(App).use(router).use(store).use(Antd).use(GoogleSignInPlugin, {
+    clientId: '180148062485-bm9fbp3s7c13adfhe3fs5ugbjatr6e0k.apps.googleusercontent.com',
+  });;
 
 export const app = initializeApp;
